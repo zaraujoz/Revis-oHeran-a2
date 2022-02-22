2afd8c9a921ff9a9a473c2228dc392b4ec633b1c
zaraujoz <matheusha@live.com>
2022-02-22 18:57:29 +0000

    Adicionando Arquivos

---
+++ Imóvel.java
@@ -0,0 +1,18 @@
+public class Imóvel {
+    private String endereco;
+    private Double preco;
+    
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public Double getPreco() {
+        return preco;
+    }
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+    
+}
